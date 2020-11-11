30fed5ec2c03e2a0ef694d051ac4d7e93ab6516d
Mokiskurve <alex.moik@gmx.at>
2020-11-11 11:10:39 +0100

    11.11.2020

--- src/November/Stundenliste1.java
+++ src/November/Stundenliste1.java
@@ -3,51 +3,34 @@ package November;
 public class Stundenliste1 {
     public static void main(String[] args) {
 
-        String[][] hourList = getList();
-        int[] hoursSum = getInts(hourList);
-        int[] salary = hoursSum(hoursSum);
-        printLoan(salary);
-    }
-
-    private static void printLoan(int[] salary) {
-        System.out.println("Alex bekommt " + salary[0] + "€ ausbezahlt.");
-        System.out.println("Daniel bekommt " + salary[1] + "€ ausbezahlt.");
-        System.out.println("Michael bekommt " + salary[2] + "€ ausbezahlt.");
-    }
-
-    private static int[] hoursSum(int[] hoursSum) {
-        int loanPerHour = 8;
-        int[] salary = new int[3];
-        for (int i = 0; i < hoursSum.length; i++) {
-            salary[i] = hoursSum[i] * loanPerHour;
+        String[][] stundenListe = getList();
+        int stundenlohn = 8;
+        int stundenPeter = 0;
+        int stundenHans = 0;
+        int stundenMonika = 0;
+        for (String[] strings : stundenListe) {
+            switch (strings[0]) {
+                case "Peter" -> stundenPeter += Integer.parseInt(strings[1]);
+                case "Hans" -> stundenHans += Integer.parseInt(strings[1]);
+                case "Monika" -> stundenMonika += Integer.parseInt(strings[1]);
+            }
         }
-        return salary;
-    }
-
-    private static int[] getInts(String[][] hourList) {
-        int[] hoursSum = new int[3];
-        for (String[] entry : hourList) {
-            String name = entry[0];
-            int hoursSumIndex = switch (name) {
-                case "Alex" -> 0;
-                case "Daniel" -> 1;
-                case "Michael" -> 2;
-                default -> 0;
-            };
-            hoursSum[hoursSumIndex] = hoursSum[hoursSumIndex] + Integer.parseInt(entry[1]);
-        }
-        return hoursSum;
+        System.out.println("Peter " + stundenPeter * stundenlohn + " €");
+        System.out.println("Hans " + stundenHans * stundenlohn + " €");
+        System.out.println("Monika " + stundenMonika * stundenlohn + " €");
     }
 
     private static String[][] getList() {
-        return new String[][]{{"Daniel", "7"},
-                {"Alex", "9"},
-                {"Michael", "8"},
-                {"Daniel", "5"},
-                {"Daniel", "3"},
-                {"Alex", "7"},
-                {"Michael", "6"},
-                {"Alex", "4"},
-                {"Michael", "5"}};
+        return new String[][]{
+                {"Peter", "15"},
+                {"Hans", "10"},
+                {"Monika", "7"},
+                {"Hans", "6"},
+                {"Peter", "-3"},
+                {"Monika", "2"},
+                {"Monika", "5"},
+                {"Hans", "3"},
+                {"Peter", "7"},
+        };
     }
 }
